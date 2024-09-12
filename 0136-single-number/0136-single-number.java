445b847a3a147d68b92c5562b44dd08404555ca9
jagathd45 <jagathd209@gmail.com>
2024-09-12 22:00:46 +0530

    Time: 1 ms (99.82%), Space: 46.2 MB (35.66%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int num = 0;
+        for(int number:nums)
+        {
+            num = num^number;
+        }
+        return num;
+    }
+}
