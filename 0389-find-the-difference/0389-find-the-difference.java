a0d2bedb4abb86fb2de9ea48244a24ca430ea0b9
jagathd45 <jagathd209@gmail.com>
2024-09-13 20:56:37 +0530

    Time: 1 ms (100%), Space: 41.7 MB (44.83%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for(char c : s.toCharArray())
+        {
+            sum1 += (int) c;
+        }
+        for(char c : t.toCharArray())
+        {
+            sum2 += (int) c;
+        }
+        return (char) (sum2-sum1);
+    }
+}
