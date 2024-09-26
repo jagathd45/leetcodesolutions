f7f12c1d91a38c6960150b65425ce665dc60a519
jagathd45 <jagathd209@gmail.com>
2024-09-26 11:23:20 +0530

    Time: 10 ms (8.97%), Space: 45.1 MB (6.79%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -14,7 +14,7 @@ class Solution {
                 return nums[i-1];
             }
         }
-        return nums[n-1];*/
+        return nums[n-1];
         long max1 = Long.MIN_VALUE;
         long max2 = Long.MIN_VALUE;
         long max3 = Long.MIN_VALUE;
@@ -36,6 +36,22 @@ class Solution {
                 max3 = num;
             }
         }
-        return max3 != Long.MIN_VALUE ? (int)max3 : (int)max1;
+        return max3 != Long.MIN_VALUE ? (int)max3 : (int)max1;*/
+        Arrays.sort(nums);
+        if(nums.length<=2)
+        return nums[nums.length-1];
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums)
+        {
+            set.add(num);
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        int n = list.size();
+        if(n<=2)
+        {
+            return list.get(n-1);
+        }
+        return list.get(n-3);                                                       
     }
 }
